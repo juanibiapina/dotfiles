4d32a075472b613016d68074dc08def1fe1077a8
Juan Ibiapina <juanibiapina@gmail.com>
2012-08-09 12:54:52 -0300

    Add git.

--- install.sh
+++ install.sh
@@ -9,3 +9,7 @@ ln -sf "$HOME/development/dotfiles/zsh/zshrc" "$HOME/.zshrc"
 
 # tmux
 ln -sf "$HOME/development/dotfiles/tmux/tmux.conf" "$HOME/.tmux.conf"
+
+# git
+ln -sf "$HOME/development/dotfiles/git/gitconfig" "$HOME/.gitconfig"
+ln -sf "$HOME/development/dotfiles/git/gitignore" "$HOME/.gitignore"
