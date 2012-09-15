64650a8ea3b3dc240186b05aa315cbb893d34079
Juan Ibiapina <juanibiapina@gmail.com>
2012-09-15 02:17:53 -0300

    Add rbenv version to prompt.

--- zsh/plugins/rbenv.sh
+++ zsh/plugins/rbenv.sh
@@ -1,6 +1,9 @@
 export PATH="/Users/juanibiapina/.rbenv/shims:${PATH}"
+
 source "/usr/local/Cellar/rbenv/0.3.0/libexec/../completions/rbenv.zsh"
+
 rbenv rehash 2>/dev/null
+
 rbenv() {
   command="$1"
   if [ "$#" -gt 0 ]; then
@@ -13,4 +16,15 @@ rbenv() {
   *)
     command rbenv "$command" "$@";;
   esac
+}
+
+# prompt function
+rbenv_prompt_info() {
+  local name
+  name=$(rbenv version-name 2> /dev/null) || return
+  if [[ $name == "system" ]]; then
+    echo ""
+  else
+    echo "%{$fg[green]%}[Ruby: $name]%{$reset_color%} "
+  fi
 }
--- zsh/themes/default.sh
+++ zsh/themes/default.sh
@@ -1,5 +1,18 @@
-PROMPT="[%{$fg[red]%}%n%{$reset_color%}@%{$fg[magenta]%}%m%{$reset_color%}:%{$fg[blue]%}%~%{$reset_color%}]
-:) "
+# Enable dynamic prompt
+setopt prompt_subst
+
+_USERNAME="%{$fg[red]%}%n%{$reset_color%}"
+_HOSTNAME="%{$fg[magenta]%}%m%{$reset_color%}"
+_CWD="%{$fg[blue]%}%~%{$reset_color%}"
+
+declare -f rbenv_prompt_info > /dev/null
+if [ $? -ne 0 ]; then
+  rbenv_prompt_info() {
+  }
+fi
+
+PROMPT='[${_USERNAME}@${_HOSTNAME}:$(rbenv_prompt_info)${_CWD}]
+:) '
 
 # display exitcode on the right when >0
 return_code="%(?..%{$fg[red]%}%? ↵%{$reset_color%})"
