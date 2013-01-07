58c9e3050d560d474a8fc99ceeab334b50c64c9d
Juan Ibiapina <juanibiapina@gmail.com>
2013-01-07 12:37:12 -0200

    Update habits plugin.
    Consider last 1000 commands.
    Show 30 most used.

--- zsh/plugins/habits.sh
+++ zsh/plugins/habits.sh
@@ -1,1 +1,1 @@
-alias habits='history -500 -1 | sed "s/^[[:space:]]*[0-9]*[[:space:]]*//" | sort | uniq -c | sort -n -r | head -n 10'
+alias habits='history -1000 -1 | sed "s/^[[:space:]]*[0-9]*[[:space:]]*//" | sort | uniq -c | sort -n -r | head -n 30'
