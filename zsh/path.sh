8db16fd65cdc34f3ec0dccb06cb4b5aef7314aab
Juan Ibiapina <juanibiapina@gmail.com>
2013-01-14 10:32:34 -0200

    Add heroku toolbelt PATH entry.

--- zsh/path.sh
+++ zsh/path.sh
@@ -1,8 +1,11 @@
-# rbenv path
-export PATH="/Users/juanibiapina/.rbenv/shims:${PATH}"
-
 # Add user bin to path
 export PATH=~/bin:$PATH
 
+# rbenv path
+export PATH="/Users/juanibiapina/.rbenv/shims:${PATH}"
+
 # Bundler path
 export PATH="vendor/bundle/binstubs:$PATH"
+
+# Added by the Heroku Toolbelt
+export PATH="/usr/local/heroku/bin:$PATH"
