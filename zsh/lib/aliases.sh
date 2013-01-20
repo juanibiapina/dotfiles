4d1bde34fcbae6f33280c5ec62017a270d5c4540
Juan Ibiapina <juanibiapina@gmail.com>
2013-01-20 10:26:07 -0200

    Fix ls colors.

--- zsh/lib/aliases.sh
+++ zsh/lib/aliases.sh
@@ -3,13 +3,5 @@ alias iptables='sudo iptables'
 
 alias webserverhere='sudo python -m SimpleHTTPServer'
 
-# enable color support of ls and also add handy aliases
-if [ -x /usr/bin/dircolors ]; then
-  test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-  alias ls='ls --color=auto'
-
-  alias grep='grep --color=auto'
-fi
-
 # cat for code
 alias catcode='highlight -s vim-dark -M'
--- zsh/plugins/ls.sh
+++ zsh/plugins/ls.sh
@@ -1,3 +1,4 @@
+alias ls='ls -G'
 alias ll='ls -alFh'
 alias la='ls -Ah'
 alias l='ls -CFh'
