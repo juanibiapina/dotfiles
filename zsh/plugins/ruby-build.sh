4d9bdd211c46387527d90b5009bc1a2149af6727
Juan Ibiapina <juanibiapina@gmail.com>
2013-03-11 12:09:34 -0300

    Add openssl dir to ruby compile options.

--- zsh/plugins/ruby-build.sh
+++ zsh/plugins/ruby-build.sh
@@ -1,3 +1,4 @@
 export CONFIGURE_OPTS="--with-readline-dir=`brew --prefix readline` --without-tcl --without-tk"
+export RUBY_CONFIGURE_OPTS="--with-openssl-dir=`brew --prefix openssl`"
 export CFLAGS=-Wno-error=shorten-64-to-32
 
