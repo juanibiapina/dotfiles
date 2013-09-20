3365cc7147b376162164c3e7e2361d696831d42f
Juan Ibiapina <juanibiapina@gmail.com>
2013-09-20 11:10:04 -0300

    Remove unnused path entries.

--- zsh/path.sh
+++ zsh/path.sh
@@ -1,17 +1,8 @@
-# Add dots to path
-export PATH=~/.dots/bin:$PATH
-
 # Add dev to path
 export PATH=~/.dev/bin:$PATH
 
-# Add user bin to path
-export PATH=~/bin:$PATH
-
 # Bundler path
 export PATH="vendor/bundle/binstubs:$PATH"
-
-# Added by the Heroku Toolbelt
-export PATH="/usr/local/heroku/bin:$PATH"
 
 # Add ts to path
 export PATH=~/development/timesheets/bin:$PATH
