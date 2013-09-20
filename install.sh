4b5a26c937cf0365744942497dace7419fbbe8b3
Juan Ibiapina <juanibiapina@gmail.com>
2013-09-20 12:15:03 -0300

    Make install script idempotent.

--- install.sh
+++ install.sh
@@ -2,26 +2,26 @@
 
 # required software
 ## basher
-"$HOME/bin/hub" clone juanibiapina/basher "$HOME/.basher"
+[[ ! -e "$HOME/.basher" ]] && "$HOME/bin/hub" clone juanibiapina/basher "$HOME/.basher"
 
 # vim
-ln -sf "$HOME/.dotfiles/vim" "$HOME/.vim"
+ln -sfn "$HOME/.dotfiles/vim" "$HOME/.vim"
 ln -sf "$HOME/.vim/vimrc" "$HOME/.vimrc"
 ln -sf "$HOME/.vim/gvimrc" "$HOME/.gvimrc"
 
 # zsh
-ln -sf "$HOME/.dotfiles/zsh/zshrc" "$HOME/.zshrc"
+ln -sf "$HOME/.dotfiles/zsh/dotsrc" "$HOME/.dotsrc"
 ln -sf "$HOME/.dotfiles/zsh/bin/zshconfig" "$HOME/bin/zshconfig"
 
 # tmux
 ln -sf "$HOME/.dotfiles/tmux/tmux.conf" "$HOME/.tmux.conf"
-ln -sf "$HOME/.dotfiles/tmux/tmuxinator" "$HOME/.tmuxinator"
+ln -sfn "$HOME/.dotfiles/tmux/tmuxinator" "$HOME/.tmuxinator"
 
 # git
 ln -sf "$HOME/.dotfiles/git/gitconfig" "$HOME/.gitconfig"
 ln -sf "$HOME/.dotfiles/git/gitignore" "$HOME/.gitignore"
 
 # gg
-ln -sf "$HOME/.dotfiles/gg" "$HOME/.gg"
+ln -sfn "$HOME/.dotfiles/gg" "$HOME/.gg"
 
 touch "$HOME/.dotfiles/.tmp/installed"
