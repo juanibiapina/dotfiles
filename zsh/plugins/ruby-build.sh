8c86cd0a37d0c719f9883d43f6dfa029fec0bdfa
Juan Ibiapina <juanibiapina@gmail.com>
2013-09-20 13:04:07 -0300

    Enable ruby-build configuration only if brew exists.

--- zsh/plugins/ruby-build.sh
+++ zsh/plugins/ruby-build.sh
@@ -1,4 +1,6 @@
-export CONFIGURE_OPTS="--with-readline-dir=`brew --prefix readline` --without-tcl --without-tk"
-export RUBY_CONFIGURE_OPTS="--with-openssl-dir=`brew --prefix openssl`"
-export CFLAGS=-Wno-error=shorten-64-to-32
+if command -v brew >/dev/null 2>&1; then
+  export CONFIGURE_OPTS="--with-readline-dir=`brew --prefix readline` --without-tcl --without-tk"
+  export RUBY_CONFIGURE_OPTS="--with-openssl-dir=`brew --prefix openssl`"
+  export CFLAGS=-Wno-error=shorten-64-to-32
+fi
 
