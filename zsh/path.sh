89033f604d13bf51199f8207d438214f4405f793
Juan Ibiapina <juanibiapina@gmail.com>
2014-07-03 17:23:23 -0300

    Remove ts from path.
    It is now managed by basher.

--- zsh/path.sh
+++ zsh/path.sh
@@ -4,9 +4,6 @@ export PATH=~/.dev/bin:$PATH
 # Bundler path
 export PATH="vendor/bundle/binstubs:$PATH"
 
-# Add ts to path
-export PATH=~/development/timesheets/bin:$PATH
-
 # Add gradle to path
 export PATH=~/Software/gradle-1.8/bin:$PATH
 
