f4c36f296396375fd9e1059d0ab2c1978a21854e
Juan Ibiapina <juanibiapina@gmail.com>
2014-11-04 11:43:20 -0200

    Use latest basher.

--- zsh/plugins/basher.sh
+++ zsh/plugins/basher.sh
@@ -1,5 +1,5 @@
 export PATH="$HOME/.basher/bin:$PATH"
 eval "$(basher init -)"
 
-source "$(basher runtime-path juanibiapina/gg)"
-source "$(basher runtime-path basherpm/antigen)"
+source "$(basher package-path juanibiapina/gg)/lib/gg.bash"
+source "$(basher package-path zsh-users/antigen)/antigen.zsh"
