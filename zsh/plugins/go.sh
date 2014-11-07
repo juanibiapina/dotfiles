f479a52d4090403d9075de4551c1e2e461dd15f7
Juan Ibiapina <juanibiapina@gmail.com>
2014-11-06 23:31:37 -0200

    Add fix for go completions.

---
+++ zsh/plugins/go.sh
@@ -0,0 +1,5 @@
+# this is required because go completions are not
+# configured to be autoloaded
+if [ -e /usr/local/share/zsh/site-functions/_go ]; then
+  source /usr/local/share/zsh/site-functions/_go
+fi
