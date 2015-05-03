fd3dcfee0f6b09926c2b449ac55afdd74168ec02
Juan Ibiapina <juanibiapina@gmail.com>
2015-05-03 13:13:42 -0300

    Add check for osx

--- zsh/plugins/run-help.sh
+++ zsh/plugins/run-help.sh
@@ -1,3 +1,5 @@
-unalias run-help
-autoload run-help
-HELPDIR=/usr/local/share/zsh/help
+if [ "$(uname -s)" = "Darwin" ]; then
+  unalias run-help
+  autoload run-help
+  HELPDIR=/usr/local/share/zsh/help
+fi
