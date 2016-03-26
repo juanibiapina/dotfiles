99842082cb3eac5d4d44c0eb5f94a624c4bcba58
Juan Ibiapina <juanibiapina@gmail.com>
2016-03-26 12:52:45 -0300

    Speed up zsh loading time

--- zsh/plugins/ruby-build.sh
+++ zsh/plugins/ruby-build.sh
@@ -1,6 +1,6 @@
 if command -v brew >/dev/null 2>&1; then
-  export CONFIGURE_OPTS="--with-readline-dir=`brew --prefix readline` --without-tcl --without-tk"
-  export RUBY_CONFIGURE_OPTS="--with-openssl-dir=`brew --prefix openssl`"
+  export CONFIGURE_OPTS="--with-readline-dir=/usr/local/opt/readline --without-tcl --without-tk"
+  export RUBY_CONFIGURE_OPTS="--with-openssl-dir=/usr/local/opt/openssl"
   export CFLAGS=-Wno-error=shorten-64-to-32
 fi
 
