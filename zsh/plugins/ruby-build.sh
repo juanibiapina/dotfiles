06852b960b7405ab9718a71e929ffa7b65c92b9f
Juan Ibiapina <juanibiapina@gmail.com>
2018-03-22 16:38:47 +0100

    Hardcode readline and openssl brew paths
    Otherwise shell takes too long to load.

--- zsh/plugins/ruby-build.sh
+++ zsh/plugins/ruby-build.sh
@@ -1,5 +1,5 @@
 if command -v brew >/dev/null 2>&1; then
-  export RUBY_CONFIGURE_OPTS="--with-readline-dir=`brew --prefix readline` --with-openssl-dir=`brew --prefix openssl` --without-tcl --without-tk"
+  export RUBY_CONFIGURE_OPTS="--with-readline-dir=/usr/local/opt/readline --with-openssl-dir=/usr/local/opt/openssl --without-tcl --without-tk"
   export CFLAGS=-Wno-error=shorten-64-to-32
 fi
 
