c6d43eef6d92e855189c87adb65dd1192edd9602
Juan Ibiapina <juanibiapina@gmail.com>
2018-07-18 21:53:01 +0200

    Clean up unused path entries

--- zsh/path.sh
+++ zsh/path.sh
@@ -4,18 +4,6 @@ export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 # User bin
 export PATH=~/bin:$PATH
 
-# dev
-export PATH=~/.dev/bin:$PATH
-
-# gradle
-export PATH=~/Software/gradle-2.2.1/bin:$PATH
-
-# SML
-export PATH=/usr/local/smlnj/bin:$PATH
-
-# Racket
-export PATH="/Applications/Racket v6.5/bin:$PATH"
-
 # Rustup
 export PATH=$HOME/.cargo/bin:$PATH
 
