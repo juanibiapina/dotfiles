66bc24a3e5e03be5734b2b7a8b116744902de682
Juan Ibiapina <juanibiapina@gmail.com>
2019-02-10 14:23:39 +0100

    Add Kitty to path

--- zsh/path.sh
+++ zsh/path.sh
@@ -10,5 +10,8 @@ export PATH=$HOME/.cargo/bin:$PATH
 # Go
 export PATH=$GOPATH/bin:$PATH
 
+# Kitty
+export PATH=/Applications/kitty.app/Contents/MacOS/:$PATH
+
 ### Added by the Heroku Toolbelt
 export PATH="/usr/local/heroku/bin:$PATH"
