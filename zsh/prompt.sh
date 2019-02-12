b686e0e25b737038a8b179e021f4251d18aa9529
Juan Ibiapina <juanibiapina@gmail.com>
2019-02-12 21:23:53 +0100

    Fix zsh prompt rendering bug
    If PROMPT has a newline character, zsh doesn't know how to redisplay it
    properly. This causes problems with the `...` characters when loading
    completions (uses `zle redisplay`) and when using `fzf` for completion
    (uses `zle reset-prompt`).
    
    This seems to be an open bug in zsh.
    
    Solution came from here:
    - https://superuser.com/questions/382503/how-can-i-put-a-newline-in-my-zsh-prompt-without-causing-terminal-redraw-issues

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -1,10 +1,10 @@
 # Enable dynamic prompt
 setopt prompt_subst
 
-export ROADRUNNER_PROMPT='#{fg(reset)};[#{fg(red)}#{username()}#{fg(reset)}@#{fg(magenta)}#{hostname()}#{fg(reset)}:;?rbenv:#{fg(green)}[Ruby #{version}] ;#{fg(blue)}#{cwd()};?git: #{fg(reset)}({#{fg(magenta)}#{tr(head)}}{ #{fg(reset)}{↓#{tr(behind)}}{↑#{tr(ahead)}}}{ {#{fg(green)}●#{tr(index)}}{#{fg(red)}+#{tr(wt)}}{#{fg(reset)}…#{tr(untracked)}}{#{fg(green)}✓#{tr(clean)}}}#{fg(reset)});#{fg(reset)}]
-:) '
+export ROADRUNNER_PROMPT='#{fg(reset)};[#{fg(red)}#{username()}#{fg(reset)}@#{fg(magenta)}#{hostname()}#{fg(reset)}:;?rbenv:#{fg(green)}[Ruby #{version}] ;#{fg(blue)}#{cwd()};?git: #{fg(reset)}({#{fg(magenta)}#{tr(head)}}{ #{fg(reset)}{↓#{tr(behind)}}{↑#{tr(ahead)}}}{ {#{fg(green)}●#{tr(index)}}{#{fg(red)}+#{tr(wt)}}{#{fg(reset)}…#{tr(untracked)}}{#{fg(green)}✓#{tr(clean)}}}#{fg(reset)});#{fg(reset)}]'
 
-PROMPT='$(~/development/roadrunner/target/release/roadrunner)'
+precmd() { print -rP "$(~/development/roadrunner/target/release/roadrunner)" }
+export PROMPT=":) "
 
 RED="%{$(tput setaf 1)%}"
 RESET="%{$(tput sgr0)%}"
