6821b9338155e9f62935dab170615fd5d4ba74d0
Juan Ibiapina <juanibiapina@gmail.com>
2019-12-01 20:04:21 -0300

    Remove unused PATH entries

--- zsh/path.sh
+++ zsh/path.sh
@@ -6,12 +6,3 @@ export PATH=~/bin:$PATH
 
 # Rustup
 export PATH=$HOME/.cargo/bin:$PATH
-
-# Go
-export PATH=$GOPATH/bin:$PATH
-
-# Kitty
-export PATH=/Applications/kitty.app/Contents/MacOS/:$PATH
-
-### Added by the Heroku Toolbelt
-export PATH="/usr/local/heroku/bin:$PATH"
