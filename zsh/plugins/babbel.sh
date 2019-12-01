df17bd471560399f19b074b4d89068dffa7294cd
Juan Ibiapina <juanibiapina@gmail.com>
2019-12-01 18:08:54 -0300

    Remove all references to git-workspace
    I can't use it because it doesn't support single repositories.
    
    https://github.com/orf/git-workspace/issues/24

--- zsh/plugins/babbel.sh
+++ zsh/plugins/babbel.sh
@@ -1,4 +1,4 @@
-export PATH="$HOME/workspace/github/lessonnine/development.cli/bin:$PATH"
-source "$HOME/workspace/github/lessonnine/development.cli/completions/babbel.zsh"
+export PATH="$HOME/development/development.cli/bin:$PATH"
+source "$HOME/development/development.cli/completions/babbel.zsh"
 
 alias bb=babbel
