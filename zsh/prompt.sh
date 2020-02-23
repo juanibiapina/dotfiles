f9c69299404bb4d220bce29aaacc747bf2991241
Juan Ibiapina <juanibiapina@gmail.com>
2020-02-23 16:33:51 +0100

    Replace roadrunner with starship

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -1,10 +1,10 @@
 # Enable dynamic prompt
 setopt prompt_subst
 
-export ROADRUNNER_PROMPT='#{fg(reset)};[#{fg(red)}#{username()}#{fg(reset)}@#{fg(magenta)}#{hostname()}#{fg(reset)}:;?rbenv:#{fg(green)}[Ruby #{version}] ;#{fg(blue)}#{cwd()};?git: #{fg(reset)}({#{fg(magenta)}#{tr(head)}}{ #{fg(reset)}{↓#{tr(behind)}}{↑#{tr(ahead)}}}{ {#{fg(green)}●#{tr(index)}}{#{fg(red)}+#{tr(wt)}}{#{fg(reset)}…#{tr(untracked)}}{#{fg(green)}✓#{tr(clean)}}}#{fg(reset)});#{fg(reset)}]'
-
-precmd() { print -rP "$(roadrunner)" }
-export PROMPT=":) "
+#export ROADRUNNER_PROMPT='#{fg(reset)};[#{fg(red)}#{username()}#{fg(reset)}@#{fg(magenta)}#{hostname()}#{fg(reset)}:;?rbenv:#{fg(green)}[Ruby #{version}] ;#{fg(blue)}#{cwd()};?git: #{fg(reset)}({#{fg(magenta)}#{tr(head)}}{ #{fg(reset)}{↓#{tr(behind)}}{↑#{tr(ahead)}}}{ {#{fg(green)}●#{tr(index)}}{#{fg(red)}+#{tr(wt)}}{#{fg(reset)}…#{tr(untracked)}}{#{fg(green)}✓#{tr(clean)}}}#{fg(reset)});#{fg(reset)}]'
+#
+#precmd() { print -rP "$(roadrunner)" }
+#export PROMPT=":) "
 
 RED="%{$(tput setaf 1)%}"
 RESET="%{$(tput sgr0)%}"
@@ -13,3 +13,5 @@ RESET="%{$(tput sgr0)%}"
 return_code="%(?..${RED}%? ↵${RESET})"
 
 RPROMPT='(${LAST_COMMAND_TIME}ms) ${return_code}${RESET}'
+
+eval "$(starship init zsh)"
