a45d3a4ba2888447ae15e69928b89393ef70399e
Juan Ibiapina <juanibiapina@gmail.com>
2020-03-20 05:00:21 +0100

    Remove go plugin
    Completions seem to be working still.

--- packages/zsh/.config/zsh/plugins/go.sh
+++
@@ -1,5 +0,0 @@
-# this is required because go completions are not
-# configured to be autoloaded
-if [ -e /usr/local/share/zsh/site-functions/_go ]; then
-  source /usr/local/share/zsh/site-functions/_go
-fi
