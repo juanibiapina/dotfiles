7e98abcf31ed4791e5d1ece039e9a2cf3442e736
Juan Ibiapina <juanibiapina@gmail.com>
2020-03-20 17:34:55 +0000

    Add arch install section

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# Link all dotfiles
 for path in packages/*; do
   # Do not process regular files, only directories
   if [ ! -d "$path" ]; then
@@ -10,3 +11,14 @@ for path in packages/*; do
 
   stow -t "${HOME}" -d packages "$package"
 done
+
+# Linux specific
+if [ "$(uname)" = "Linux" ]; then
+  # Build arch packages
+  for pkg in assets/arch/*; do
+    (
+      cd "$pkg"
+      makepkg -ci --noconfirm
+    )
+  done
+fi
