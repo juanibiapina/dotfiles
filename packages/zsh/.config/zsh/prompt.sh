1c3ee13c90d67ababa8fb590fe720b72301c3230
Juan Ibiapina <juanibiapina@gmail.com>
2020-03-20 04:32:53 +0100

    Remove one level of indirection when rendering prompt

--- packages/zsh/.config/zsh/prompt.sh
+++ packages/zsh/.config/zsh/prompt.sh
@@ -1,1 +1,5 @@
-eval "$(starship init zsh)"
+# This is the code suggested by starship, but since it includes this extra
+# unecessary process spawn, I've included the resolved full init directly.
+#eval "$(starship init zsh)"
+
+source <(starship init zsh --print-full-init)
