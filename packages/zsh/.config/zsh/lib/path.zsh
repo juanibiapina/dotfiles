85e76cf5b121efabc877742a9745aa7461a2f6ce
Juan Ibiapina <juanibiapina@gmail.com>
2020-05-20 07:06:23 +0200

    Configure NPM global install path per user
    This allows `npm install -g` without sudo.

--- packages/zsh/.config/zsh/lib/path.zsh
+++ packages/zsh/.config/zsh/lib/path.zsh
@@ -4,5 +4,8 @@ export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 # User bin
 export PATH=~/bin:$PATH
 
+# Npm
+export PATH="$HOME/resources/node_modules/bin:$PATH"
+
 # Rustup
 export PATH=$HOME/.cargo/bin:$PATH
---
+++ packages/zsh/.config/zsh/plugins/npm.sh
@@ -0,0 +1,2 @@
+# Set the location of globally installed node packages
+export npm_config_prefix=~/resources/node_modules
