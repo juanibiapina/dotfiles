1857c590745b625e980752d88b9db2bf9088aa5c
Juan Ibiapina <juanibiapina@gmail.com>
2020-05-20 08:17:30 +0200

    Prevent stow from linking directories

--- scripts/link-dotfiles.bash
+++ scripts/link-dotfiles.bash
@@ -5,9 +5,35 @@ for path in packages/*; do
     continue
   fi
 
-  package=${path##*/}
+  # Directory where dotfiles are stored
+  prefix="packages"
 
-  stow -t "${HOME}" -d packages "$package"
+  # Name of current package (first level directory inside prefix)
+  name=${path##*/}
+
+  # Loop through all config files recursively
+  for file in `find "$path/"`; do
+    # Remove `prefix/name` to get the final relative path of the config file or directory
+    file="${file#"$prefix/$name/"}"
+
+    # The first entry is always `prefix/name/` which gets reduced to nothing
+    if [ -z "$file" ]; then
+      continue
+    fi
+
+    # Process file
+    source_path="$prefix/$name/$file"
+    destination_path="$HOME/$file"
+    if [ -d "$source_path" ]; then
+      # In case it's a directory, create it, otherwise stow will link the
+      # directory instead of the file, causing later added files to be tracked
+      # by git
+      mkdir -p "$destination_path"
+    fi
+  done
+
+  # Install dotfiles with stow
+  stow -t "${HOME}" -d packages "$name"
 done
 
 echo "Creating OS Specific dotfiles"
