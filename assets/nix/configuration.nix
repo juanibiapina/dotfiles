def784a8d0eb4844c9c5864cf9931c09d6eaed84
Juan Ibiapina <juanibiapina@gmail.com>
2021-01-01 13:09:13 +0100

    Set Nix timezone

--- assets/nix/configuration.nix
+++ assets/nix/configuration.nix
@@ -24,7 +24,7 @@
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
+  time.timeZone = "Europe/Berlin";
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
