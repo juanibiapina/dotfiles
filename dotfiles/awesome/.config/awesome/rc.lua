71f17b66c0f65af881182a293def7aa8b1fb3af2
Juan Ibiapina <juanibiapina@gmail.com>
2021-01-06 04:29:10 +0100

    awesome: Start with wibar hidden

--- dotfiles/awesome/.config/awesome/rc.lua
+++ dotfiles/awesome/.config/awesome/rc.lua
@@ -196,7 +196,7 @@ awful.screen.connect_for_each_screen(function(s)
     }
 
     -- Create the wibox
-    s.mywibox = awful.wibar({ position = "top", screen = s })
+    s.mywibox = awful.wibar({ position = "top", screen = s, visible = false })
 
     -- Add widgets to the wibox
     s.mywibox:setup {
