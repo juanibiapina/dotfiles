e19e2a16a1dbdafcb7d2a46820a44392bd78519a
Juan Ibiapina <juanibiapina@gmail.com>
2021-01-06 04:11:05 +0100

    awesome: Add window placement rules

--- dotfiles/awesome/.config/awesome/rc.lua
+++ dotfiles/awesome/.config/awesome/rc.lua
@@ -493,9 +493,12 @@ awful.rules.rules = {
       }, properties = { titlebars_enabled = true }
     },
 
-    -- Set Firefox to always map on the tag named "2" on screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { screen = 1, tag = "2" } },
+    { rule = { class = "Alacritty" },
+      properties = { screen = 1, tag = "1" } },
+    { rule = { class = "qutebrowser" },
+      properties = { screen = 1, tag = "2" } },
+    { rule = { class = "Firefox Developer Edition" },
+      properties = { screen = 1, tag = "6" } },
 }
 -- }}}
 
