2394e958a392073609297d7ad495af27467c5b44
Juan Ibiapina <juanibiapina@gmail.com>
2021-01-06 03:34:04 +0100

    awesome: Add autostart programs

--- dotfiles/awesome/.config/awesome/rc.lua
+++ dotfiles/awesome/.config/awesome/rc.lua
@@ -562,3 +562,6 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+-- Autostart programs
+awful.spawn.with_shell("~/.config/awesome/autostart.sh")
