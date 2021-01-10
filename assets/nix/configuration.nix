11a67714d25e6b7b0c0c052156024ff510d9a115
Juan Ibiapina <juanibiapina@gmail.com>
2021-01-10 17:20:52 +0100

    nix: Enable steam

--- assets/nix/configuration.nix
+++ assets/nix/configuration.nix
@@ -169,6 +169,9 @@
   # Enable gnupg agent
   programs.gnupg.agent.enable = true;
 
+  # Enable steam
+  programs.steam.enable = true;
+
   # Enable flatpak package manager
   services.flatpak.enable = true;
   xdg.portal.enable = true;
