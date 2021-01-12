f165ea771baba54189684078ab924b101e510669
Juan Ibiapina <juanibiapina@gmail.com>
2021-01-12 19:49:55 +0100

    Revert "fzf: Comment out key bindings since I'm not using them"
    Turns out I am using them.
    
    This reverts commit bc03969c61a5d3cf8deb754b6613d5bc671f42ae.

--- assets/zsh/plugins/fzf.sh
+++ assets/zsh/plugins/fzf.sh
@@ -5,4 +5,4 @@ export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 # Key bindings
 # ------------
-#source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+source "/usr/local/opt/fzf/shell/key-bindings.zsh"
