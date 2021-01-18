9b3380089e13c879d2729cfe8e886d8cdcc2fc0c
Juan Ibiapina <juanibiapina@gmail.com>
2021-01-19 00:00:11 +0100

    nix: Enable virtualbox

--- assets/nix/configuration.nix
+++ assets/nix/configuration.nix
@@ -199,6 +199,9 @@
   # Enable docker
   virtualisation.docker.enable = true;
 
+  # Enable virtual box
+  virtualisation.virtualbox.host.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
