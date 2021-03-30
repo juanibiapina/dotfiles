105ce4c817ba54255e0c566757c1224d0220d01e
Juan Ibiapina <juanibiapina@gmail.com>
2021-03-30 23:35:37 +0200

    Load default session when autostarting qutebrowser

--- dotfiles/awesome/.config/awesome/autostart.sh
+++ dotfiles/awesome/.config/awesome/autostart.sh
@@ -12,6 +12,6 @@ xrandr --output HDMI-A-0 --auto --primary --output DisplayPort-2 --auto --scale 
 run alacritty
 run dropbox
 run firefox-devedition
-run qutebrowser
+run qutebrowser -r default
 run slack
 run spotifywm
