a5ea267f946ee83aa3da53ea9576898585afdad5
Juan Ibiapina <juanibiapina@gmail.com>
2021-06-11 17:39:06 +0200

    nixos: Open barrier port

--- assets/nix/configuration.nix
+++ assets/nix/configuration.nix
@@ -242,7 +242,7 @@
   services.openssh.enable = true;
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 24800 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
