bf5206dea370f505979e66806e18b6237ada0a1b
Juan Ibiapina <juanibiapina@gmail.com>
2021-10-14 01:02:15 +0200

    awesome: Hide borders when clients are maximized

--- dotfiles/awesome/.config/awesome/rc.lua
+++ dotfiles/awesome/.config/awesome/rc.lua
@@ -174,6 +174,20 @@ end
 -- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
 screen.connect_signal("property::geometry", set_wallpaper)
 
+-- Hide border when clients are maximized
+screen.connect_signal("arrange", function (s)
+    local max = s.selected_tag.layout.name == "max"
+    local only_one = #s.tiled_clients == 1 -- use tiled_clients so that other floating windows don't affect the count
+    -- but iterate over clients instead of tiled_clients as tiled_clients doesn't include maximized windows
+    for _, c in pairs(s.clients) do
+        if (max or only_one) and not c.floating or c.maximized then
+            c.border_width = 0
+        else
+            c.border_width = beautiful.border_width
+        end
+    end
+end)
+
 awful.screen.connect_for_each_screen(function(s)
   -- Wallpaper
   set_wallpaper(s)
