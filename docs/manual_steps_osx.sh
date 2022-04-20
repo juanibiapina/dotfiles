104c610f9de8c1e9f6edf442597ac9ded788d214
Juan Ibiapina <juanibiapina@gmail.com>
2022-04-20 03:20:29 +0200

    docs: Update OSX manual installation steps

--- docs/manual_steps_osx.sh
+++ docs/manual_steps_osx.sh
@@ -33,28 +33,21 @@ defaults write com.apple.dock autohide -int 1
 # Reduce Dock size
 defaults write com.apple.dock tilesize -int 43
 
-# Setup dotfiles
-
 # Allow brew zsh to be a shell
 which zsh | sudo tee -a /etc/shells
 
 # Change default shell to zsh
 chsh -s `which zsh`
 
-# Disable whatsapp notifications
 # Configure number of workspaces
 # Disable rearranging spaces in mission control
 # Configure keyboard shortcuts for displays
 # Set timezone
-# Set clock to display date
 # Configure display positions
 # Increase trackpad speed
 
-# Install rustup
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
-# Clone basher
-# Link basher in home folder
 # Install juanibiapina/dev
+
+# install nix-direnv and flakes with nix-env
 
 # Logout and login again
