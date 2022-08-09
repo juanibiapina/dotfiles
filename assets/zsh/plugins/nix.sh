5f063de0deb0e2560a0a9b9fa048bf2b6da3f267
Juan Ibiapina <juanibiapina@gmail.com>
2022-08-09 14:05:12 +0200

    Fix nix setup on OSX
    For some reason OSX keeps rewriting `/etc/zshrc` back to the original.
    This is better anyway because it's under my control.

--- assets/zsh/plugins/nix.sh
+++ assets/zsh/plugins/nix.sh
@@ -1,1 +1,8 @@
 export NIXPKGS_ALLOW_UNFREE=1
+
+if [ "$(uname -s)" = "Darwin" ]; then
+  # nix setup that is normally done by /etc/zshrc on osx
+  if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+    . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+  fi
+fi
