904bafb5ec7a9b3a3f4ddb81be22dde39298fc15
Juan Ibiapina <juanibiapina@gmail.com>
2022-12-02 20:15:50 +0100

    nix: Clean up comments

--- assets/nix/configuration.nix
+++ assets/nix/configuration.nix
@@ -32,10 +32,6 @@
   networking.interfaces.enp4s0.useDHCP = true;
   #networking.interfaces.wlp7s0.useDHCP = true;
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
   # console = {
