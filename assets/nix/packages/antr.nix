6a47c290e409710d71a8ff4e6e1e4d2c931c8ccd
Juan Ibiapina <juanibiapina@gmail.com>
2023-02-01 12:19:59 +0100

    nixos: Update antr to v1.0.1

--- assets/nix/packages/antr.nix
+++ assets/nix/packages/antr.nix
@@ -2,19 +2,19 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "antr";
-  version = "1.0.0";
+  version = "1.0.1";
 
   src = fetchFromGitHub {
     owner = "juanibiapina";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-PCmSFikkkyPRmjk6kwvNPzn4wtOBIru8L6L66xu4Msc=";
+    sha256 = "sha256-fqKCaSzQFbvO3uMpP5HUZ9gGPGyZht96efyeLPEB+ro=";
   };
 
   nativeBuildInputs = [ openssl pkg-config ];
   buildInputs = [ openssl ];
 
-  cargoSha256 = "sha256-mFqgTXgFG6wooZ7JEcvB/TQ+XnJ/xYJPTNMeYUIXjS4=";
+  cargoSha256 = "sha256-az9IKJhZQNst/4URXqZipmtyFejWbHhv+5bVY5kRwwY=";
 
   meta = with lib; {
     description = "A simple to use file system event watcher that runs arbitrary commands";
