e5b6b9f77abf7e0d4154be3f38d4b373ff069393
Juan Ibiapina <juanibiapina@gmail.com>
2023-04-20 17:51:48 +0200

    nix: Migrate rest of applications to new autostart

--- dotfiles/awesome/.config/awesome/autostart.sh
+++ dotfiles/awesome/.config/awesome/autostart.sh
@@ -1,17 +1,3 @@
 #!/usr/bin/env bash
 
-function run {
-  if ! pgrep -f $1 ;
-  then
-    $@&
-  fi
-}
-
-# Disable screensaver and monitor power saving settings
-#xset s off -dpms
-
 xrandr --output DP-4 --auto --pos 0x0 --primary --output DP-2 --auto --pos 2560x0
-
-run pcloud
-run slack
-run udiskie --tray
