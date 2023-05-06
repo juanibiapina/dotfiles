49fdba4668ff15f482d7704746ac6cc71dea5b88
Juan Ibiapina <juanibiapina@gmail.com>
2023-05-06 19:43:30 +0200

    neovim: Configure LSP

--- dotfiles/nvim/.config/nvim/init.lua
+++ dotfiles/nvim/.config/nvim/init.lua
@@ -13,12 +13,34 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- set leader to space
+-- this needs to happen before lazy is setup
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- setup plugins
 require("lazy").setup({
   spec = {
+    -- per project configuration files
+    "folke/neoconf.nvim",
+
+    { -- LSP
+      'neovim/nvim-lspconfig',
+      dependencies = {
+        -- Automatically install LSPs to stdpath for neovim
+        {
+          "williamboman/mason.nvim",
+          build = ":MasonUpdate",
+        },
+        'williamboman/mason-lspconfig.nvim',
+
+        -- Status updates for LSP
+        'j-hui/fidget.nvim',
+
+        -- Additional configuration for neovim lua plugin development
+        'folke/neodev.nvim',
+      },
+    },
+
     { -- keymap documentation
       "folke/which-key.nvim",
       config = function()
@@ -45,7 +67,6 @@ require("lazy").setup({
     "junegunn/fzf.vim",
 
     -- Utilities
-    "dense-analysis/ale",
     "tpope/vim-surround",
     "editorconfig/editorconfig-vim",
     "tpope/vim-repeat",
@@ -178,6 +199,88 @@ require("lazy").setup({
       },
     },
   },
+})
+
+-- Setup neoconf
+require("neoconf").setup({})
+
+-- Setup fidget
+require("fidget").setup({})
+
+-- LSP settings.
+--  This function gets run when an LSP connects to a particular buffer.
+local on_attach = function(_, bufnr)
+  local nmap = function(keys, func, desc)
+    if desc then
+      desc = 'LSP: ' .. desc
+    end
+
+    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+  end
+
+  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+  nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+  nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+  nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+  -- See `:help K` for why this keymap
+  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+
+  -- Lesser used LSP functionality
+  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+  nmap('<leader>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, '[W]orkspace [L]ist Folders')
+
+  -- Create a command `:Format` local to the LSP buffer
+  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+    vim.lsp.buf.format()
+  end, { desc = 'Format current buffer with LSP' })
+end
+
+-- Setup neovim lua configuration
+require('neodev').setup()
+
+-- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+-- Language servers to be installed using mason
+-- https://github.com/williamboman/mason-lspconfig.nvim#available-lsp-servers
+local servers = {
+  tsserver = {},
+  -- Do not install solargraph since it's a gem. Do this per project instead.
+  -- solargraph = {},
+
+  lua_ls = {
+    Lua = {
+      workspace = { checkThirdParty = false },
+      telemetry = { enable = false },
+    },
+  },
+}
+
+require('mason').setup()
+require('mason-lspconfig').setup({
+  ensure_installed = vim.tbl_keys(servers),
+
+  handlers = {
+    function(server_name)
+      require('lspconfig')[server_name].setup {
+        capabilities = capabilities,
+        on_attach = on_attach,
+        settings = servers[server_name],
+      }
+    end,
+  }
 })
 
 local function source_vimscript(filepath)
