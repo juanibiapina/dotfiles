f7f4c6298f317b860b3b59749c44ab4803d8d45b
Juan Ibiapina <juanibiapina@gmail.com>
2023-11-29 14:19:23 +0100

    ci: Try an even simpler config to see if the build works

--- assets/nix/default.nix
+++ assets/nix/default.nix
@@ -1,11 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
 
-let
-  nixpkgsConfig = { allowUnfree = true; };
-  configuredPkgs = import <nixpkgs> { config = nixpkgsConfig; };
-in
 {
-  system = configuredPkgs.lib.recurseIntoAttrs (
-    configuredPkgs.nixos [ ./configuration.nix ]
+  system = pkgs.lib.recurseIntoAttrs (
+    pkgs.nixos [ ./configuration.nix ]
   );
 }
