4a093351c5e69c7bf724cba46b2527a9094b14b8
Juan Ibiapina <juanibiapina@gmail.com>
2023-11-30 13:36:59 +0100

    nixos: Build NixOS using flakes

--- assets/nix/default.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-{
-  system = pkgs.lib.recurseIntoAttrs (
-    pkgs.nixos [ ./configuration.nix ]
-  );
-}
---
+++ assets/nix/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+
+    nixpkgs_pcloud_working = {
+      url = "github:NixOS/nixpkgs/e3652e0735fbec227f342712f180f4f21f0594f2";
+    };
+  };
+
+  outputs = { self, nixpkgs, nixpkgs_pcloud_working, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        config.allowUnfree = true;
+      };
+      specialArgs = {
+        nixpkgs_pcloud_working = import nixpkgs_pcloud_working {
+          system = "x86_64-linux";
+          config.allowUnfree = true;
+        };
+      };
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
