d31ca3ff63443cd649e22b21b6d1aaab4b1c45bf
Juan Ibiapina <juanibiapina@gmail.com>
2023-12-07 23:18:30 +0100

    man: Fix colors
    https://github.com/sharkdp/bat/issues/2568

--- assets/zsh/plugins/bat.sh
+++ assets/zsh/plugins/bat.sh
@@ -1,2 +1,9 @@
+# Configure bat as man pager
 export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+
+# Fix issue with colors in man pages
+# https://github.com/sharkdp/bat/issues/2568
+export MANROFFOPT="-c"
+
+# Set bat theme
 export BAT_THEME="Solarized (dark)"
