89ad4482f483710d82126910b44dd16fcf913ad8
Juan Ibiapina <juanibiapina@gmail.com>
2023-12-16 18:47:18 +0100

    neovim: Remove conflicting shortcut
    This made <leader>w take too long to save.

--- dotfiles/nvim/.config/nvim/conf/auto/lsp.lua
+++ dotfiles/nvim/.config/nvim/conf/auto/lsp.lua
@@ -20,7 +20,6 @@ local on_attach = function(_, bufnr)
   nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
   nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
   nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
   -- See `:help K` for why this keymap
   nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
