cad699061485dc92581e9f5ef2bcf19fae4b9e9e
Juan Ibiapina <juanibiapina@gmail.com>
2023-12-19 16:31:37 +0100

    chore: Migrate development shell to devenv.sh

--- flake.nix
+++ flake.nix
@@ -1,21 +1,55 @@
 {
   inputs = {
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-    };
-    nixpkgs = {
-      url = "nixpkgs";
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in
-        {
-          devShells.default = import ./shell.nix { inherit pkgs; };
-        }
-      );
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      packages = forEachSystem (system: {
+        devenv-up = self.devShells.${system}.default.config.procfileScript;
+      });
+
+      devShells = forEachSystem
+        (system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+          {
+            default = devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [
+                {
+                  # https://devenv.sh/reference/options/
+                  packages = with pkgs; [
+                    ruby
+                    vim-full
+                  ];
+
+                  enterShell = ''
+                    # install gems locally
+                    mkdir -p .local/nix-gems
+                    export GEM_HOME=$PWD/.local/nix-gems
+                    export GEM_PATH=$GEM_HOME
+                    export PATH=$GEM_HOME/bin:$PATH
+
+                    # add local bin directory to path
+                    export PATH=$PWD/bin:$PATH
+                  '';
+
+                  processes.run.exec = "hello";
+                }
+              ];
+            };
+          });
+    };
 }
