61b461325ff51b8c6a73a4ff64aa68acc64661b6
Juan Ibiapina <juanibiapina@gmail.com>
2024-01-18 15:35:49 +0100

    hotkeys: Add shortcut to explain things on the screen

--- dotfiles/xbindkeys/.xbindkeysrc.scm
+++ dotfiles/xbindkeys/.xbindkeysrc.scm
@@ -1,2 +1,3 @@
 (xbindkey '(control space) "zsh -l -i -c 'dev rofi launcher'")
 (xbindkey '(Print) "zsh -l -i -c 'dev screenshot'")
+(xbindkey '(control Print) "zsh -l -i -c 'dev ai explain-screen'")
