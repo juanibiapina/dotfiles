099ff585369ce133c3ed9e17b911412dddee8ef0
Juan Ibiapina <juanibiapina@gmail.com>
2024-01-30 17:45:08 +0100

    rofi: Add global shortcut for the emoji picker

--- dotfiles/xbindkeys/.xbindkeysrc.scm
+++ dotfiles/xbindkeys/.xbindkeysrc.scm
@@ -1,3 +1,4 @@
 (xbindkey '(control space) "zsh -l -i -c 'dev rofi launcher'")
 (xbindkey '(Print) "zsh -l -i -c 'dev screenshot'")
 (xbindkey '(control Print) "zsh -l -i -c 'dev ai explain-screen'")
+(xbindkey '(control shift e) "zsh -l -i -c 'rofi -modi emoji -show emoji'")
