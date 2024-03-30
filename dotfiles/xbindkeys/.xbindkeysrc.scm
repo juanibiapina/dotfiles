3a6b9f7c225ebe4ea71060e58f94c64776c0d67c
Juan Ibiapina <juanibiapina@gmail.com>
2024-03-30 20:26:46 +0100

    xbindkeys: Add support for space mode bindings

--- dotfiles/xbindkeys/.xbindkeysrc.scm
+++ dotfiles/xbindkeys/.xbindkeysrc.scm
@@ -1,14 +1,35 @@
-; space
-(xbindkey '(Alt space) "zsh -l -i -c 'dev rofi launcher'")
-;(xbindkey '(control space) "")
+; switch current set of bindings to a new one
+(define (switch-mode bindings)
+  ; clear bindings
+  (ungrab-all-keys)
+  (remove-all-keys)
 
-; print screen
-(xbindkey '(Print) "zsh -l -i -c 'dev screenshot'")
-(xbindkey '(control Print) "zsh -l -i -c 'dev ai explain-screen'")
+  ; setup new bindings
+  (bindings)
 
-; ctrl+shift
-(xbindkey '(control shift e) "zsh -l -i -c 'rofi -modi emoji -show emoji'")
+  ; grab keys again
+  (grab-all-keys))
 
-; programs
-(xbindkey '(control KP_1) "vivaldi")
+(define (normal-mode-bindings)
+  ; launcher
+  (xbindkey '(Alt space) "zsh -l -i -c 'dev rofi launcher'")
 
+  ; print screen
+  (xbindkey '(Print) "zsh -l -i -c 'dev screenshot'")
+  (xbindkey '(control Print) "zsh -l -i -c 'dev ai explain-screen'")
+
+  ; ctrl+shift
+  (xbindkey '(control shift e) "zsh -l -i -c 'rofi -modi emoji -show emoji'")
+
+  ; programs
+  (xbindkey '(control KP_1) "vivaldi")
+
+  ; switch to space mode
+  (xbindkey-function '(control space) (lambda () (switch-mode space-mode-bindings))))
+
+(define (space-mode-bindings)
+  ; switch to normal mode
+  (xbindkey-function '(Escape) (lambda () (switch-mode normal-mode-bindings))))
+
+; start in normal mode
+(switch-mode normal-mode-bindings)
