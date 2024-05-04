c59e68cb7e9d8bb3cdc4bd7e942f8a295d2a36fc
Juan Ibiapina <juanibiapina@gmail.com>
2024-05-04 15:47:06 +0200

    zsh: Don't error when basher is not installed

--- assets/zsh/plugins/basher.sh
+++ assets/zsh/plugins/basher.sh
@@ -1,3 +1,10 @@
-export BASHER_ROOT="$HOME/workspace/basherpm/basher"
+# init basher if installed
+if type basher > /dev/null; then
+  # basher is installed in a non default location
+  export BASHER_ROOT="$HOME/workspace/basherpm/basher"
 
-eval "$(basher init - zsh)"
+  # load shell hook
+  eval "$(basher init - zsh)"
+fi
+
+
