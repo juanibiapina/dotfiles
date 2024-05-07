a213ec080d9bfbe9fe04a503d93de1b9afb1ed2a
Juan Ibiapina <juanibiapina@gmail.com>
2024-05-08 00:53:22 +0200

    nix: Merge flakes into one

--- flake.nix
+++ flake.nix
@@ -1,44 +1,117 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    systems.url = "github:nix-systems/default";
-    devenv.url = "github:cachix/devenv";
-  };
+  description = "Systems configuration";
 
   nixConfig = {
     extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
     extra-substituters = "https://devenv.cachix.org";
   };
 
-  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
-    let
-      forEachSystem = nixpkgs.lib.genAttrs (import systems);
-    in
-    {
-      packages = forEachSystem (system: {
-        devenv-up = self.devShells.${system}.default.config.procfileScript;
-      });
-
-      devShells = forEachSystem
-        (system:
-          let
-            pkgs = nixpkgs.legacyPackages.${system};
-          in
-          {
-            default = devenv.lib.mkShell {
-              inherit inputs pkgs;
-              modules = [
-                {
-                  packages = with pkgs; [
-                    vim-full
-                  ];
-
-                  languages.ruby.enable = true;
-                  languages.nix.enable = true;
-                  languages.lua.enable = true;
-                }
-              ];
-            };
-          });
+  inputs = {
+    devenv = {
+      url = "github:cachix/devenv";
     };
+
+    systems = {
+      url = "github:nix-systems/default";
+    };
+
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+
+    nixpkgs_pcloud_working = {
+      url = "github:NixOS/nixpkgs/e3652e0735fbec227f342712f180f4f21f0594f2";
+    };
+
+    neovim-nightly-overlay = {
+      url = "github:nix-community/neovim-nightly-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    sub = {
+      url = "github:juanibiapina/sub";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ self, nixpkgs, nixpkgs_pcloud_working, neovim-nightly-overlay, nix-darwin, sub, systems, devenv, ... }: {
+    checks.x86_64-linux.desktop = self.nixosConfigurations."desktop".config.system.build.toplevel;
+    checks.x86_64-linux.mini = self.nixosConfigurations."mini".config.system.build.toplevel;
+
+    nixosConfigurations.desktop = nixpkgs.lib.nixosSystem rec {
+      system = "x86_64-linux";
+
+      pkgs = import nixpkgs {
+        system = system;
+        config.allowUnfree = true;
+        config.permittedInsecurePackages = [
+          "electron-25.9.0" # for obsidian
+        ];
+        overlays = [ neovim-nightly-overlay.overlay ];
+      };
+
+      specialArgs = {
+        inherit sub;
+
+        nixpkgs_pcloud_working = import nixpkgs_pcloud_working {
+          system = system;
+          config.allowUnfree = true;
+        };
+      };
+
+      modules = [ ./assets/nix/hosts/desktop/configuration.nix ];
+    };
+
+    nixosConfigurations.mini = nixpkgs.lib.nixosSystem rec {
+      system = "x86_64-linux";
+
+      pkgs = import nixpkgs {
+        system = system;
+        config.allowUnfree = true;
+        overlays = [ neovim-nightly-overlay.overlay ];
+      };
+
+      specialArgs = {
+        inherit sub;
+      };
+
+      modules = [ ./assets/nix/hosts/mini/configuration.nix ];
+    };
+
+    darwinConfigurations."babbel" = nix-darwin.lib.darwinSystem {
+      specialArgs = {
+        inherit self;
+      };
+
+      modules = [ ./assets/nix/hosts/babbel/configuration.nix ];
+    };
+
+    devShells = nixpkgs.lib.genAttrs (import systems) (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+
+          modules = [
+            {
+              packages = with pkgs; [
+                vim-full
+              ];
+
+              languages.ruby.enable = true;
+              languages.nix.enable = true;
+              languages.lua.enable = true;
+            }
+          ];
+        };
+      }
+    );
+  };
 }
