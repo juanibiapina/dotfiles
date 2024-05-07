3182cd8914035080f8209f0b7dafbea9b1cd1690
Juan Ibiapina <juanibiapina@gmail.com>
2024-05-08 01:47:14 +0200

    nix: Add nix-community cache

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "Systems configuration";
 
   nixConfig = {
-    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-    extra-substituters = "https://devenv.cachix.org";
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
+    extra-substituters = "https://devenv.cachix.org https://nix-community.cachix.org";
   };
 
   inputs = {
