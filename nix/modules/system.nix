1e542ba93e81caab69e3d640df2bd1e90ff0e65e
Juan Ibiapina <juanibiapina@gmail.com>
2024-05-25 23:06:27 +0200

    syncthing: Add pixel and digitalgarden config

--- nix/modules/system.nix
+++ nix/modules/system.nix
@@ -73,6 +73,10 @@
     openDefaultPorts = true;
 
     settings = {
+      options = {
+        urAccepted = -1;
+      };
+
       devices = {
         "desktop" = {
           id = "RSLPDC6-GSD5IBK-CILWGCL-66KG7EJ-H6X4ANG-NA6UHGN-YFSDHGB-BDP2XAG";
@@ -80,12 +84,20 @@
         "mini" = {
           id = "GH5VODQ-6LTTY7O-NEJQNYG-DTQE3L5-SL7L66X-Z6LIRPQ-QBBU44N-62BDBQU";
         };
+        "pixel-juan" = {
+          id = "DILKHP4-RVHVKA6-VKEIFGS-OKDH2FT-VB7JILH-I3EQMSG-G6X35BT-F72KAAV";
+        };
       };
 
       folders = {
         "secrets" = {
           path = "/home/juan/Sync/secrets";
           devices = [ "desktop" "mini" ];
+        };
+
+        "digitalgarden" = {
+          path = "/home/juan/Sync/DigitalGarden";
+          devices = [ "desktop" "mini" "pixel-juan" ];
         };
       };
     };
