e6363bbbe94233c8d38c4f657578cf98605f901f
Juan Ibiapina <juanibiapina@gmail.com>
2024-09-20 15:41:50 +0200

    nvim: Use terminal previewers with telescope
    This is slower but at least it works. This can be reverted once nvim
    treesitter support is in a good state.

--- dotfiles/nvim/.config/nvim/conf/auto/telescope.lua
+++ dotfiles/nvim/.config/nvim/conf/auto/telescope.lua
@@ -7,5 +7,12 @@ require("telescope").setup({
         ["<C-k>"] = require("telescope.actions").move_selection_previous,
       },
     },
+
+    -- set default previewers to use the terminal because sometimes treesitter highlighting doesn't work
+    -- mainly for ruby at this point
+    file_previewer = require'telescope.previewers'.cat.new,
+    grep_previewer = require'telescope.previewers'.vimgrep.new,
+    qflist_previewer = require'telescope.previewers'.qflist.new,
   },
 })
+
