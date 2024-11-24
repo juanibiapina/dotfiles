455f3677677ff9ccb05c9beb6eaa0a6edb14425f
Juan Ibiapina <juanibiapina@gmail.com>
2024-11-24 20:15:21 +0100

    stylix: Enable solarized colors in system level config

--- nix/hosts/desktop/configuration.nix
+++ nix/hosts/desktop/configuration.nix
@@ -42,12 +42,12 @@
 
   stylix = {
     enable = true;
-    autoEnable = false;
+    autoEnable = true;
     image = pkgs.fetchurl {
       url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
       sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
     };
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/solarized-dark.yaml";
   };
 
   # Power management
