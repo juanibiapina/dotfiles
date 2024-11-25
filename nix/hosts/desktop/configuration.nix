82037d3f54b98edd2feaa1b0fd5de6ec868203e2
Juan Ibiapina <juanibiapina@gmail.com>
2024-11-25 22:39:18 +0100

    stylix: Set dark mode

--- nix/hosts/desktop/configuration.nix
+++ nix/hosts/desktop/configuration.nix
@@ -48,6 +48,7 @@
       sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
     };
     base16Scheme = "${pkgs.base16-schemes}/share/themes/solarized-dark.yaml";
+    polarity = "dark";
     fonts = {
       sizes.terminal = 16;
 
