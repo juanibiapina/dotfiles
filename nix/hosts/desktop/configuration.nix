fa8dc2da278cc9b6cb80ad083beb6ff4b7e481ce
Juan Ibiapina <juanibiapina@gmail.com>
2024-11-27 09:39:31 +0100

    desktop: Use waybar instead of swaybar

--- nix/hosts/desktop/configuration.nix
+++ nix/hosts/desktop/configuration.nix
@@ -220,7 +220,7 @@
 
   programs.sway = {
     enable = true;
-    extraPackages = with pkgs; [ grim pulseaudio swayidle swaylock wl-clipboard ];
+    extraPackages = with pkgs; [ grim pulseaudio swayidle swaylock wl-clipboard waybar ];
     wrapperFeatures.gtk = true;
   };
 
