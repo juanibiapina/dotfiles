5c9038bc058a77636418ffe28795bdf61d5d2f49
Juan Ibiapina <juanibiapina@gmail.com>
2024-11-28 12:29:39 +0100

    stylix: Set wallpaper (even though this isn't used)
    Sway overrides the wallpaper.

--- nix/hosts/desktop/configuration.nix
+++ nix/hosts/desktop/configuration.nix
@@ -38,10 +38,7 @@
   stylix = {
     enable = true;
     autoEnable = true;
-    image = pkgs.fetchurl {
-      url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
-      sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
-    };
+    image = ./assets/wallpapers/wp2272565-kindness-wallpapers.png;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/solarized-dark.yaml";
     polarity = "dark";
     fonts = {
