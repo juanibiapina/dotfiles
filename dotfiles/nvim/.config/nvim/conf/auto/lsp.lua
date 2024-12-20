0fc300d71a83a874c3d7c29a4fe42594fb85fa40
Juan Ibiapina <juanibiapina@gmail.com>
2024-12-20 16:50:08 +0100

    neovim: Move rest of lsp shortcuts to global
    K is already defined by default by neovim.

--- dotfiles/nvim/.config/nvim/conf/auto/lsp.lua
+++ dotfiles/nvim/.config/nvim/conf/auto/lsp.lua
@@ -1,21 +1,6 @@
 -- Status updates for LSP
 require("fidget").setup({})
 
---  This function gets run when an LSP connects to a particular buffer.
-local on_attach = function(_, bufnr)
-  local nmap = function(keys, func, desc)
-    if desc then
-      desc = 'LSP: ' .. desc
-    end
-
-    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-  end
-
-  -- See `:help K` for why this keymap
-  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-end
-
 -- configure the LSP servers
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 local servers = {
@@ -34,13 +19,11 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 for _, server in ipairs(servers) do
   require('lspconfig')[server].setup {
     capabilities = capabilities,
-    on_attach = on_attach,
   }
 end
 
 require('lspconfig')["rust_analyzer"].setup {
   capabilities = capabilities,
-  on_attach = on_attach,
   settings = {
     ["rust-analyzer"] = {
       files = {
