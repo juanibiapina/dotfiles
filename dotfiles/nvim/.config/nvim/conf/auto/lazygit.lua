1e5d3b78056e710ca1795345b1f6b799ae3d5acf
Juan Ibiapina <juanibiapina@gmail.com>
2024-12-22 14:07:09 +0100

    neovim: Fix lazygit terminal detection

--- dotfiles/nvim/.config/nvim/conf/auto/lazygit.lua
+++ dotfiles/nvim/.config/nvim/conf/auto/lazygit.lua
@@ -4,15 +4,10 @@ local nvim_terminal_augroup = vim.api.nvim_create_augroup('nvim_terminal', { cle
 vim.api.nvim_create_autocmd({ 'TermClose' }, {
   group = nvim_terminal_augroup,
   desc = 'Automatically close buffers after running lazygit',
+  pattern = 'term://*:lazygit',
   callback = function(args)
     if vim.v.event.status == 0 then
-      -- TODO: Find a better way to detect lazygit, this also breaks when running :terminal
-      local info = vim.api.nvim_get_chan_info(vim.bo[args.buf].channel)
-      local argv = info.argv or {}
-      -- argv should be { ".../zsh", "-c", "lazygit" }
-      if #argv == 3 and argv[3] == 'lazygit' then
-        vim.cmd({ cmd = 'bdelete', args = { args.buf }, bang = true })
-      end
+      vim.cmd({ cmd = 'bdelete', args = { args.buf }, bang = true })
     end
   end,
 })
