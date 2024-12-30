ca2f859c39bd43f4be58531d0581c949af8d4a8c
Juan Ibiapina <juanibiapina@gmail.com>
2024-12-30 17:51:06 +0100

    nix: Configure substituters for all machines

--- nix/hosts/mini/configuration.nix
+++ nix/hosts/mini/configuration.nix
@@ -16,6 +16,17 @@
   networking.hostId = "1855342b";
   networking.hostName = "mini";
 
+  nix.settings = {
+    substituters = [
+      "https://devenv.cachix.org"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   # console = {
   #   font = "Lat2-Terminus16";
   #   keyMap = "us";
