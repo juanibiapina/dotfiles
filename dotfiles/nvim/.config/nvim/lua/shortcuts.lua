6f1b29b48c4d016954af4bc9ec216ef3da04a813
Juan Ibiapina <juanibiapina@gmail.com>
2025-01-17 08:57:42 +0100

    neovim: Open lighttree vertical splits on the left side

--- dotfiles/nvim/.config/nvim/lua/shortcuts.lua
+++ dotfiles/nvim/.config/nvim/lua/shortcuts.lua
@@ -102,7 +102,7 @@ map('<Leader>llr', ':lua vim.lsp.buf.rename()', 'LSP: Rename symbol')
 -- n: lighttree
 map('<leader>n<space>', ':lua require("yazi").yazi({log_level = vim.log.levels.DEBUG})', 'Open file manager')
 map('<leader>nf', ':LightTreeFind', 'Find current file in Lighttree')
-map('<leader>ns', ':vsplit<CR>:LightTree', 'Open Lighttree in a vertical split')
+map('<leader>ns', ':leftabove vsplit<CR>:LightTree', 'Open Lighttree in a vertical split')
 map('<leader>nt', ':LightTree', 'Open Lighttree in current window')
 
 -- nn: disable search highlight
