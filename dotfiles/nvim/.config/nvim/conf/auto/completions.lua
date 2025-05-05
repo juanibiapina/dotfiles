9526f393a331d35f06455e40c95904540daf33a5
Juan Ibiapina <juanibiapina@gmail.com>
2025-05-05 19:37:20 +0200

    neovim: Rework completions

--- dotfiles/nvim/.config/nvim/conf/auto/completions.lua
+++ dotfiles/nvim/.config/nvim/conf/auto/completions.lua
@@ -1,20 +1,30 @@
 local cmp = require("cmp");
 
 cmp.setup({
+  preselect = cmp.PreselectMode.None,
+  experimental = {
+    ghost_text = false,
+  },
   snippet = {
     expand = function(args)
-        -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-        -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-        require('snippy').expand_snippet(args.body) -- For `snippy` users.
-        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-      end,
+      -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      require('snippy').expand_snippet(args.body) -- For `snippy` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+    end,
   },
   mapping = cmp.mapping.preset.insert({
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-l>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<CR>'] = cmp.mapping({
+      i = function(fallback)
+        if cmp.visible() and cmp.get_active_entry() then
+          cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+        else
+          fallback()
+        end
+      end,
+    }),
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
@@ -22,7 +32,5 @@ cmp.setup({
     --{ name = 'luasnip' }, -- For luasnip users.
     --{ name = 'ultisnips' }, -- For ultisnips users.
     { name = 'snippy' }, -- For snippy users.
-  }, {
-    { name = 'buffer' }, -- fallback when other sources aren't available
   }),
 })
