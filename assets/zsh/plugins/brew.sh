b81c0e717284f2b26123963f0607fb6d92c7ed56
Juan Ibiapina <juanibiapina@gmail.com>
2025-05-15 17:23:32 +0200

    brew: Disable quarantine in any run of brew install

--- assets/zsh/plugins/brew.sh
+++ assets/zsh/plugins/brew.sh
@@ -1,4 +1,5 @@
 if [ "$(uname -s)" = "Darwin" ]; then
   eval "$(/opt/homebrew/bin/brew shellenv)"
   fpath=(/usr/local/share/zsh-completions $fpath)
+  export HOMEBREW_CASK_OPTS="--no-quarantine"
 fi
