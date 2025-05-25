ff8ca3ab87557f76af0d242c15f87138b020547a
Juan Ibiapina <juanibiapina@gmail.com>
2025-05-25 13:14:49 +0200

    zsh: Remove old ruby-build options
    These aren't correct or recommended anymore.

--- assets/zsh/plugins/ruby-build.sh
+++
@@ -1,5 +0,0 @@
-if command -v brew >/dev/null 2>&1; then
-  export RUBY_CONFIGURE_OPTS="--with-readline-dir=/usr/local/opt/readline --with-openssl-dir=/usr/local/opt/openssl@1.1 --without-tcl --without-tk"
-  export CFLAGS=-Wno-error=shorten-64-to-32
-fi
-
