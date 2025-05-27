5182c0fd5ceffeb5063f5665c648cb268277bd8a
Juan Ibiapina <juanibiapina@gmail.com>
2025-05-27 12:14:51 +0200

    neovim: Reorganize highlights and fix float colors

--- dotfiles/nvim/.config/nvim/lua/highlights.lua
+++ dotfiles/nvim/.config/nvim/lua/highlights.lua
@@ -1,11 +1,36 @@
-vim.api.nvim_command('highlight! link CmpItemAbbrDeprecated Comment')
-vim.api.nvim_command('highlight! link CmpItemAbbrMatch Search')
-vim.api.nvim_command('highlight! link CmpItemAbbrMatchFuzzy CmpItemAbbrMatch')
-vim.api.nvim_command('highlight! link CmpItemKindVariable Identifier')
-vim.api.nvim_command('highlight! link CmpItemKindInterface CmpItemKindVariable')
-vim.api.nvim_command('highlight! link CmpItemKindText CmpItemKindVariable')
-vim.api.nvim_command('highlight! link CmpItemKindFunction Function')
-vim.api.nvim_command('highlight! link CmpItemKindMethod CmpItemKindFunction')
-vim.api.nvim_command('highlight! link CmpItemKindKeyword Keyword')
-vim.api.nvim_command('highlight! link CmpItemKindProperty CmpItemKindKeyword')
-vim.api.nvim_command('highlight! link CmpItemKindUnit CmpItemKindKeyword')
+local colors = {
+  '#073642',
+  '#dc322f',
+  '#859900',
+  '#b58900',
+  '#268bd2',
+  '#d33682',
+  '#2aa198',
+  '#eee8d5',
+  '#002b36',
+  '#cb4b16',
+  '#586e75',
+  '#657b83',
+  '#839496',
+  '#6c71c4',
+  '#93a1a1',
+  '#fdf6e3',
+}
+
+local c = {
+  red = colors[2],
+  green = colors[3],
+  blue = colors[5],
+
+  yellow = colors[4],
+  magenta = colors[6],
+  cyan = colors[7],
+
+  fg = colors[8],
+  bg = colors[9],
+
+  fg_alternate = colors[16],
+  bg_alternate = colors[1],
+}
+
+vim.api.nvim_set_hl(0, "NormalFloat", { fg = c.fg_alternate, bg = c.bg_alternate })
