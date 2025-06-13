9cc26c26339f140a9aead5c5521764998f07e928
Juan Ibiapina <juanibiapina@gmail.com>
2025-06-13 19:14:14 +0200

    direnv: allowlist direnv configs

--- nix/modules/direnv.nix
+++ nix/modules/direnv.nix
@@ -3,5 +3,10 @@
     enable = true;
     silent = true;
     nix-direnv.enable = true;
+    settings = {
+      whitelist = {
+        exact = ["~/workspace/contentful" "~/workspace/ninetailed-inc"];
+      };
+    };
   };
 }
