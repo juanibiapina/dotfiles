ecb0d3254a7315ed01d940c4b40247fc3427efff
Juan Ibiapina <juanibiapina@gmail.com>
2025-06-20 08:03:30 +0200

    mini: Enable prometheus node exporter

--- nix/modules/prometheus.nix
+++ nix/modules/prometheus.nix
@@ -1,3 +1,28 @@
+{ config, ... }:
+
 {
-  services.prometheus.enable = true;
+  services.prometheus = {
+    enable = true;
+    port = 9090;
+    globalConfig.scrape_interval = "10s";
+
+    scrapeConfigs = [
+      {
+        job_name = "node";
+        static_configs = [{
+          targets = [ "localhost:${toString config.services.prometheus.exporters.node.port}" ];
+        }];
+      }
+    ];
+  };
+
+  services.prometheus.exporters.node = {
+    enable = true;
+    port = 9091;
+    enabledCollectors = [ "systemd" ];
+    # node_exporter --help
+    extraFlags = [ "--collector.ethtool" "--collector.softirqs" "--collector.tcpstat" ];
+  };
+
+  networking.firewall.allowedTCPPorts = [ 9090 ];
 }
