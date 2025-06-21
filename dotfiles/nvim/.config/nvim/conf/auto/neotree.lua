1cc5e445e2f9d37a742c61e71de503a45f392d90
Juan Ibiapina <juanibiapina@gmail.com>
2025-06-21 14:11:01 +0200

    neovim: Detach neotree cwd from vim's cwd

--- dotfiles/nvim/.config/nvim/conf/auto/neotree.lua
+++ dotfiles/nvim/.config/nvim/conf/auto/neotree.lua
@@ -1,5 +1,11 @@
 require("neo-tree").setup({
   filesystem = {
+    bind_to_cwd = true, -- true creates a 2-way binding between vim's cwd and neo-tree's root
+    cwd_target = {
+      sidebar = "none", -- sidebar is when position = left or right
+      current = "none" -- current is when position = current
+    },
+
     filtered_items = {
       visible = true, -- when true, they will just be displayed differently than normal items
       hide_dotfiles = false,
