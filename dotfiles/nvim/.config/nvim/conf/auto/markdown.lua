cc0a98dcf3435abea3ae4e73955731750b634f6b
Juan Ibiapina <juanibiapina@gmail.com>
2025-06-30 18:56:56 +0200

    neovim: Improve markdown rendering

---
+++ dotfiles/nvim/.config/nvim/conf/auto/markdown.lua
@@ -0,0 +1,31 @@
+require('render-markdown').setup {
+  heading = {
+    sign = false,
+    border = true,
+    below = "▔",
+    above = "▁",
+    left_pad = 0,
+    position = "left",
+    icons = {
+      "█ ",
+      "██ ",
+      "███ ",
+      "████ ",
+      "█████ ",
+      "██████ ",
+    },
+  },
+  code = {
+    sign = false,
+    border = "thin",
+    position = "right",
+    width = "block",
+    above = "▁",
+    below = "▔",
+    language_left = "█",
+    language_right = "█",
+    language_border = "▁",
+    left_pad = 1,
+    right_pad = 1,
+  },
+}
