155f3623104eb6fbbcaee41398ab99821141521a
Juan Ibiapina <juanibiapina@gmail.com>
2025-07-14 18:06:58 +0200

    wrangler: Configure ninetailed api token

--- nix/hosts/mac16/home-manager.nix
+++ nix/hosts/mac16/home-manager.nix
@@ -65,6 +65,7 @@
     '';
 
     "workspace/ninetailed-inc/.envrc".text = ''
+      export CLOUDFLARE_API_TOKEN="$CLOUDFLARE_API_TOKEN_NINETAILED"
       export EMAIL="$EMAIL_CONTENTFUL"
       export GITHUB_TOKEN="$GITHUB_TOKEN_CONTENTFUL"
       export GIT_SSH_COMMAND='ssh -i ~/.ssh/id_ed25519_contentful'
