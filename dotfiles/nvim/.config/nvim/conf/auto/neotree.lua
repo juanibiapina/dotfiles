fdcc34190fcdd98d7c236bd9b340a6756ec8e8bc
Juan Ibiapina <juanibiapina@gmail.com>
2025-07-21 15:12:49 +0200

    neovim: Neotree follow current open file

--- dotfiles/nvim/.config/nvim/conf/auto/neotree.lua
+++ dotfiles/nvim/.config/nvim/conf/auto/neotree.lua
@@ -1,9 +1,15 @@
 require("neo-tree").setup({
   filesystem = {
     bind_to_cwd = true, -- true creates a 2-way binding between vim's cwd and neo-tree's root
+
     cwd_target = {
       sidebar = "none", -- sidebar is when position = left or right
       current = "none" -- current is when position = current
+    },
+
+    follow_current_file = {
+      enabled = true,         -- Enable auto-sync with current buffer
+      leave_dirs_open = true, -- Keep directories expanded when revealing files
     },
 
     window = {
@@ -17,5 +23,12 @@ require("neo-tree").setup({
       hide_dotfiles = false,
       hide_gitignored = true,
     }
-  }
+  },
+
+  buffers = {
+    follow_current_file = {
+      enabled = true,         -- Enable auto-sync with current buffer
+      leave_dirs_open = true, -- Keep directories expanded when revealing files
+    },
+  },
 })
