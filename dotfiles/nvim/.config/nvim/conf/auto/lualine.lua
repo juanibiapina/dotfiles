7123f3c64a679844688338e5b60aa6181d279b25
Juan Ibiapina <juanibiapina@gmail.com>
2025-08-01 15:31:00 +0200

    neovim: Display path in status line

--- dotfiles/nvim/.config/nvim/conf/auto/lualine.lua
+++ dotfiles/nvim/.config/nvim/conf/auto/lualine.lua
@@ -34,7 +34,7 @@ require('lualine').setup {
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'diagnostics'},
-    lualine_c = {'filename'},
+    lualine_c = {{'filename', path = 1}},
     lualine_x = {'lsp_status', 'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
@@ -42,7 +42,7 @@ require('lualine').setup {
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
+    lualine_c = {{'filename', path = 1}},
     lualine_x = {'location'},
     lualine_y = {},
     lualine_z = {}
