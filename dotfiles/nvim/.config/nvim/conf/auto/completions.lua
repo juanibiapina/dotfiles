4cb1b6e13e2a6d89a92894fc10c753a6e310ec91
Juan Ibiapina <juanibiapina@gmail.com>
2025-08-02 17:31:18 +0200

    neovim: Remove comments

--- dotfiles/nvim/.config/nvim/conf/auto/completions.lua
+++ dotfiles/nvim/.config/nvim/conf/auto/completions.lua
@@ -7,10 +7,7 @@ cmp.setup({
   },
   snippet = {
     expand = function(args)
-      -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-      -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-      require('snippy').expand_snippet(args.body) -- For `snippy` users.
-      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      require('snippy').expand_snippet(args.body)
     end,
   },
   mapping = cmp.mapping.preset.insert({
@@ -28,10 +25,7 @@ cmp.setup({
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
-    --{ name = 'vsnip' }, -- For vsnip users.
-    --{ name = 'luasnip' }, -- For luasnip users.
-    --{ name = 'ultisnips' }, -- For ultisnips users.
-    { name = 'snippy' }, -- For snippy users.
+    { name = 'snippy' },
   }),
 })
 
