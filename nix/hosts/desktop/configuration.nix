2f17f0b487a31783ff98f5d1cafd27b67bac187b
Juan Ibiapina <juanibiapina@gmail.com>
2025-10-06 09:41:44 +0200

    desktop: Remove deprecated config
    Failed assertions:
           - The option definition `hardware.amdgpu.amdvlk' in
    `/nix/store/97g5n14jcaks064j67wiggk5nyy8qp73-source/nix/hosts/desktop/configuration.nix'
    no longer has any effect; please remove it.
           'amdvlk' has been removed. The replacement driver RADV, part of
    Mesa, is enabled by default.

--- nix/hosts/desktop/configuration.nix
+++ nix/hosts/desktop/configuration.nix
@@ -163,13 +163,8 @@
 
   hardware = {
     graphics = {
-        enable = true;
-        enable32Bit = true;
-    };
-
-    amdgpu.amdvlk = {
-        enable = true;
-        support32Bit.enable = true;
+      enable = true;
+      enable32Bit = true;
     };
   };
 
