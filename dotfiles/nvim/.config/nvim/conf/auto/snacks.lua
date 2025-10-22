51f85beffc81a553790b7b87896d11935de372d4
Juan Ibiapina <juanibiapina@gmail.com>
2025-10-22 16:11:41 +0200

    neovim: configure dashboard using snacks

--- dotfiles/nvim/.config/nvim/conf/auto/snacks.lua
+++ dotfiles/nvim/.config/nvim/conf/auto/snacks.lua
@@ -5,4 +5,12 @@ require("snacks").setup {
       enabled = false, -- disable indent animation
     },
   },
+  dashboard = {
+    enabled = true,
+    -- Exclude startup section since we use vim-plug, not lazy.nvim
+    sections = {
+      { section = "header" },
+      { section = "keys", gap = 1, padding = 1 },
+    },
+  },
 }
