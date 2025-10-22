46243f2cbe07ef74083c2eb490971f26b7d41b59
Juan Ibiapina <juanibiapina@gmail.com>
2025-10-22 16:13:07 +0200

    neovim: Remove outdated markdown preview plugin
    It's not maintained since 2023. Specifically, mermaid 11 support is
    missing.

--- dotfiles/nvim/.config/nvim/lua/plugins.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins.lua
@@ -74,7 +74,6 @@ Plug("tpope/vim-fugitive")
 -- Markdown
 Plug("mzlogin/vim-markdown-toc")
 Plug('MeanderingProgrammer/render-markdown.nvim') -- pretty markdown rendering
-Plug("iamcco/markdown-preview.nvim", { ['do'] = 'cd app && npx --yes yarn install' })
 
 -- Notes
 Plug("juanibiapina/notes.nvim")
