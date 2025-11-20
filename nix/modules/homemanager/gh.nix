f9f68c1da821d695d3f19c718a1a0ee239695481
Juan Ibiapina <juanibiapina@gmail.com>
2025-11-20 18:02:40 +0100

    install gh extension to cleanup notifications

--- nix/modules/homemanager/gh.nix
+++ nix/modules/homemanager/gh.nix
@@ -1,8 +1,42 @@
 { pkgs, ... }:
 
+let
+  gh-cleanup-notifications = pkgs.stdenv.mkDerivation {
+    pname = "gh-cleanup-notifications";
+    version = "0.1.0";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "awendt";
+      repo = "gh-cleanup-notifications";
+      rev = "main";
+      sha256 = "sha256-Ghpo/IMBIDTX64dRxkEwdLGPfhrM54x01/WTyiRP3fo=";
+    };
+
+    nativeBuildInputs = [ pkgs.makeWrapper ];
+    buildInputs = [ pkgs.nodejs_20 ];
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp -r . $out/bin/
+      chmod +x $out/bin/gh-cleanup-notifications
+
+      wrapProgram $out/bin/gh-cleanup-notifications \
+        --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.nodejs_20 pkgs.moreutils ]}
+    '';
+
+    meta = with pkgs.lib; {
+      description = "GitHub CLI extension to cleanup notifications";
+      homepage = "https://github.com/awendt/gh-cleanup-notifications";
+      platforms = platforms.all;
+    };
+  };
+in
 {
   programs.gh = {
     enable = true;
-    extensions = [ pkgs.gh-notify ];
+    extensions = [
+      pkgs.gh-notify
+      gh-cleanup-notifications
+    ];
   };
 }
