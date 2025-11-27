bc26cbf59644a52f32535da3a146b869b18d965b
Juan Ibiapina <juanibiapina@gmail.com>
2025-11-27 11:52:08 +0100

    configure gh for contentful-labs

--- nix/hosts/mac16/home-manager.nix
+++ nix/hosts/mac16/home-manager.nix
@@ -64,6 +64,15 @@
       export GIT_SSH_COMMAND='ssh -i ~/.ssh/id_ed25519_contentful'
     '';
 
+    "workspace/contentful-labs/.envrc".text = ''
+      export CODING_AGENT="codex"
+      export EMAIL="$EMAIL_CONTENTFUL"
+      export GITHUB_TOKEN="$GITHUB_TOKEN_CONTENTFUL"
+      export GIT_AUTHOR_EMAIL="$EMAIL_CONTENTFUL"
+      export GIT_COMMITTER_EMAIL="$EMAIL_CONTENTFUL"
+      export GIT_SSH_COMMAND='ssh -i ~/.ssh/id_ed25519_contentful'
+    '';
+
     "workspace/ninetailed-inc/.envrc".text = ''
       export CODING_AGENT="codex"
       export CLOUDFLARE_API_TOKEN="$CLOUDFLARE_API_TOKEN_NINETAILED"
