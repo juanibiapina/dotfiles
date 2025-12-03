2eb658347af22c30461ac3285fc74e66492119ad
Juan Ibiapina <juanibiapina@gmail.com>
2025-12-03 21:39:09 +0100

    update manual steps

--- docs/manual_steps_osx.sh
+++ docs/manual_steps_osx.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
 
+# This script should run after the manual steps from the flash drive
+
 wait_for_input() {
   echo
   echo "Press any key to continue..."
   read -n 1 -s
 }
-
-# change hostname (System Settings > General > About)
-echo "Go to System Settings > General > About and change the hostname to the desired one."
-wait_for_input
-
-# install command line tools
-xcode-select --install
 
 # install brew (https://brew.sh/)
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -19,46 +14,33 @@ xcode-select --install
 # configure terminal to use brew
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-# install dotfiles
-mkdir -p "$HOME"/workspace/juanibiapina
-(
-  cd "$HOME"/workspace/juanibiapina || exit
-  git clone https://github.com/juanibiapina/dotfiles.git
-  cd dotfiles || exit
-  make
-)
-
 # install nix
 sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install)
 
 # run nix darwin for the first time
 (
   cd "$HOME"/workspace/juanibiapina/dotfiles || exit
-  nix run nix-darwin/master#darwin-rebuild -- --flake . switch
+  sudo nix run --extra-experimental-features nix-command --extra-experimental-features flakes nix-darwin/master#darwin-rebuild -- --flake . switch
+)
+
+# terminal restart needed
+
+# install dotfiles
+(
+  cd "$HOME"/workspace/juanibiapina/dotfiles || exit
+  make
 )
 
 # set zsh as default shell
 chsh -s /run/current-system/sw/bin/zsh
 
-# Install pcloud (not available on nix or brew)
-echo "Install pcloud"
-wait_for_input
-
-# import secrets
+# import secrets (requires syncthing to be synced, but could import from flash drive instead)
+dev secrets import-master-key
 
 # init git crypt for dotfiles
 echo "Init git crypt for dotfiles."
+# git-crypt unlock
 wait_for_input
 
-# Open karabiner-elements for the first time and enable background services
-echo "Open Karabiner-Elements for the first time and enable background services."
-wait_for_input
-
-# Setup dropbox
-
-# Logout and login again
+# restart
 echo "Logout and login again to apply changes."
-
-# configs missing:
-- create 10 workspaces
-- shortcuts for workspaces
