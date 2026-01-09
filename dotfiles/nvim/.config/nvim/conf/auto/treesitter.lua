13e8010e66c14d834ef9e1ceb84d835bd54eec1f
Juan Ibiapina <juanibiapina@gmail.com>
2026-01-09 14:05:29 -0300

    neovim: fix treesitter config after flake upgrade

--- dotfiles/nvim/.config/nvim/conf/auto/treesitter.lua
+++ dotfiles/nvim/.config/nvim/conf/auto/treesitter.lua
@@ -1,21 +1,6 @@
 -- https://github.com/nvim-treesitter/nvim-treesitter
 
-require('nvim-treesitter.configs').setup {
-  -- Don't automatically install missing parsers
-  -- This is handled by nix
-  auto_install = false,
+require('nvim-treesitter').setup()
 
-  highlight = {
-    enable = true,
-  },
-
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "<c-n>",
-      node_incremental = "<c-n>",
-      scope_incremental = "<c-s>",
-      node_decremental = "<c-p>",
-    },
-  },
-}
+-- NOTE: Incremental selection was removed from nvim-treesitter
+-- See: https://github.com/MeanderingProgrammer/treesitter-modules.nvim for alternatives
