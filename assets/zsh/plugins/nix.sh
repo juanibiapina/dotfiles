c3fd6d538f2aedddf57d7f0026599777a30170e2
Juan Ibiapina <juanibiapina@gmail.com>
2026-01-25 22:23:44 +0100

    zsh: remove redundant nix-daemon.sh sourcing that caused duplicate PATH entries

--- assets/zsh/plugins/nix.sh
+++ assets/zsh/plugins/nix.sh
@@ -1,17 +1,6 @@
 # allow unfree packages when running with `nix run nixpkgs#<package>`
 export NIXPKGS_ALLOW_UNFREE=1
 
-# reset the guard so nix-daemon.sh gets applied properly
-unset __ETC_PROFILE_NIX_SOURCED
-
-if [ "$(uname -s)" = "Darwin" ]; then
-  # nix setup that is normally done by /etc/zshrc on osx
-  if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
-    . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
-  fi
-elif [ "$(uname -s)" = "Linux" ]; then
-  # nix setup for linux
-  if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
-    . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
-  fi
-fi
+# Note: Nix PATH setup is handled by:
+# - macOS: /etc/zshenv (nix-darwin) sources set-environment
+# No need to source nix-daemon.sh here - it causes duplicate PATH entries.
