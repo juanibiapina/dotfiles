d16d40dcefa4aa0882a4f8d6a5f42f4cbe4b921e
Juan Ibiapina <juanibiapina@gmail.com>
2026-06-08 21:36:50 +0200

    mini: manage gpg-agent config through NixOS
    Move gpg-agent settings (pinentry, cache TTL, no-grab) from
    ~/.gnupg/gpg-agent.conf into the NixOS module so pinentry-curses
    is always resolved from the Nix store.

--- nix/hosts/mini/modules/system.nix
+++ nix/hosts/mini/modules/system.nix
@@ -67,7 +67,16 @@
   security.sudo.wheelNeedsPassword = false;
 
   # Enable gnupg agent
-  programs.gnupg.agent.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    pinentryPackage = pkgs.pinentry-curses;
+    settings = {
+      no-grab = "";
+      no-allow-external-cache = "";
+      default-cache-ttl = 86400;
+      max-cache-ttl = 86400;
+    };
+  };
 
   # Enable direnv
   programs.direnv = {
