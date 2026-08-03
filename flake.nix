7b101a13e99cdf4fc74cce98478092cd81149456
Juan Ibiapina <juanibiapina@gmail.com>
2026-08-03 10:22:20 +0200

    debug: add set -x and verbose patchShebangs to find artifacts CI failure

--- flake.nix
+++ flake.nix
@@ -188,12 +188,13 @@
         artifacts = pkgs.runCommand "artifacts" {
           nativeBuildInputs = [ pkgs.bash pkgs.coreutils pkgs.findutils pkgs.git pkgs.gnugrep pkgs.jq sub.packages.${system}.default ];
         } ''
+          set -x
           export PATH=${pkgs.lib.makeBinPath [ pkgs.bash pkgs.coreutils pkgs.findutils pkgs.git pkgs.gnugrep pkgs.jq sub.packages.${system}.default ]}:$PATH
           export HOME=$TMPDIR/home
           mkdir -p "$HOME" "$TMPDIR/bin"
           cp -r ${self} "$TMPDIR/src"
           chmod -R u+w "$TMPDIR/src"
-          patchShebangs "$TMPDIR/src/dotfiles/bin/bin" "$TMPDIR/src/cli" >/dev/null
+          patchShebangs "$TMPDIR/src/dotfiles/bin/bin" "$TMPDIR/src/cli" 2>&1
           ln -s "$TMPDIR/src/dotfiles/bin/bin/dev" "$TMPDIR/bin/dev"
           export PATH="$TMPDIR/bin:$PATH"
           DEV="$TMPDIR/src/dotfiles/bin/bin/dev" bash "$TMPDIR/src/cli/test/artifacts"
